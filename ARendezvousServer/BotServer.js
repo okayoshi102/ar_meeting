c0e25e84793a2d74a0f6a4ae63c0cd595a39fa67
tomonori takada <tomo@tp-takatomo-mac.ics.nitech.ac.jp>
2018-10-23 14:22:15 +0900

    delete access token

---
+++ ARendezvousServer/BotServer.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 5000;
+// const PORT = 5000;
+
+const config = {
+	channelSecret: '',
+	channelAccessToken: ''
+};
+class BotServer{
+	//サーバ起動
+	static run(){
+		this.express=express();
+		this.http=require("http").Server(this.express)
+		this.express.post('/webhook', line.middleware(config), (req, res) => {
+			console.log(req.body.events);
+			console.log(BotServer.handleEvent);
+			console.log("Bot Server に Post された");
+			Promise
+			.all(req.body.events.map(BotServer.handleEvent))
+			.then((result) => res.json(result));
+		});
+
+		this.client = new line.Client(config);
+
+		console.log("BotServer起動");
+		this.http.listen(PORT, () => {
+			console.log(`listening on *:${PORT}`);
+		});
+	}
+	//LINEに送信されたメッセージを受信
+	static handleEvent(event) {
+		console.log("get message");
+		if(event.message.type == "location"){
+			//位置情報が送信された
+			BotServer.notifyDestinationUpdate({latitude:event.message.latitude,longitude:event.message.longitude})
+			return BotServer.client.replyMessage(event.replyToken, {
+				type: 'text',
+				text: "arendezvous://?"
+			})
+		}
+		if (event.type !== 'message' || event.message.type !== 'text') {
+			return Promise.resolve(null);
+		}
+		console.log(event.message.text);
+		if(event.message.text != "！ついたで" && event.message.text != "!ついたで"){
+			// return BotServer.client.replyMessage(event.replyToken, {
+			// 	type: 'text',
+			// 	text: event.message.text
+			// })
+			return Promise.resolve(null)
+		}
+		//キーワードが送信された
+		return BotServer.client.replyMessage(event.replyToken, {
+			type: 'text',
+			text: "どこおるん?",
+			"quickReply": {
+				"items": [
+					{
+						"type": "action",
+						"action": {
+							"type":"location",
+							"label":"ここやで"
+						}
+					},
+					{
+						"type": "message",
+						"action": {
+							"type": "message",
+							"label": "内緒",
+							"text": "内緒"
+						}
+					},
+					{
+						"type": "message",
+						"action": {
+							"type": "message",
+							"label": "てへぺろ",
+							"text": "てへぺろ"
+						}
+					}
+				]
+			}
+		});
+	}
+	//目的地が更新された時の通知先を設定
+	static setDestinationObserver(aObserver){
+		this.destinationObserver=aObserver
+	}
+	//目的地更新を通知
+	static notifyDestinationUpdate(aDestination){
+		console.log("目的地更新通知");
+		this.destinationObserver(aDestination)
+	}
+}
+module.exports = BotServer;
---
+++ jpHacks2018-team-toralab/jpHacks2018-team-toralab/SocketClient.swift
@@ -0,0 +1,87 @@
+//
+//  SocketClient.swift
+//  jpHacks2018-team-toralab
+//
+//  Created by tomonori takada on 2018/10/18.
+//  Copyright © 2018年 jpHacks2018-team-toralab. All rights reserved.
+//
+
+import Foundation
+import SocketIO
+import GoogleMaps
+
+class SocketClient{
+    static private var mManager:SocketManager!
+    static private var mSocket:SocketIOClient!
+    //オブザーバ
+//    static private var mUpdateLocationObserver:[((CLLocationCoordinate2D,String,String)->(),String)]!=[]
+    static private var mUpdateDestinationObserver:[((CLLocationCoordinate2D)->(),String)]!=[]
+    //サーバに接続する
+    static func connect(){
+        mManager=SocketManager(socketURL: URL(string: "")!, config: [.log(true), .compress])
+        mSocket=mManager.defaultSocket
+        print("サーバにアクセスしています")
+        mSocket.on("connected", callback: {(_,_) in
+            print("サーバに接続しました")
+        })
+        mSocket.on("updateLocation", callback: {(arg,_) in
+            print("目的地が更新されました")
+            let tArg=(arg[0] as! NSDictionary)
+            var tDic=Dictionary<String,Float>()
+            tDic["latitude"]=(tArg["latitude"]! as! NSNumber).floatValue
+            tDic["longitude"]=(tArg["longitude"]! as! NSNumber).floatValue
+            let tLocation=CLLocationCoordinate2D.convertFrom(tDic)
+            notifyUpdateDestination(tLocation)
+        })
+        mSocket.connect()
+    }
+    //現在地が更新されるたびにサーバに通知する
+    static private func setSendChangeLocationEvent(){
+        LocationManager.setUpdateLocationObserver({aLocation in
+            mSocket.emit("updateLocation", aLocation.toDic())
+        }, "sendLocationToServer")
+    }
+
+    //目的地更新オブザーバ追加
+    static func setUpdateDestinationObserver(_ aFunction:@escaping (CLLocationCoordinate2D)->(),
+                                          _ aName:String=""){
+        mUpdateDestinationObserver.append((aFunction,aName))
+    }
+    //目的地更新オブザーバ削除
+    static func removeUpdateDestinationObserver(_ aName:String){
+        for i in 0..<mUpdateDestinationObserver.count{
+            let tObserver=mUpdateDestinationObserver[i]
+            if(tObserver.1 != aName){continue}
+            mUpdateDestinationObserver.remove(at: i)
+            return
+        }
+    }
+    //オブザーバ呼び出し
+    private static func notifyUpdateDestination(_ aLocation:CLLocationCoordinate2D){
+        let tObserverList=mUpdateDestinationObserver!
+        for tObserver in tObserverList{
+            tObserver.0(aLocation)
+        }
+    }
+//    //ユーザの座標更新オブザーバ追加
+//    static func setUpdateLocationObserver(_ aFunction:@escaping (CLLocationCoordinate2D,String,String)->(),
+//                                          _ aName:String=""){
+//        mUpdateLocationObserver.append((aFunction,aName))
+//    }
+//    //ユーザの座標更新オブザーバ削除
+//    static func removeUpdateLocationObserver(_ aName:String){
+//        for i in 0..<mUpdateLocationObserver.count{
+//            let tObserver=mUpdateLocationObserver[i]
+//            if(tObserver.1 != aName){continue}
+//            mUpdateLocationObserver.remove(at: i)
+//            return
+//        }
+//    }
+//    //ユーザの座標更新オブザーバ呼び出し
+//    private static func notifyUpdateLocation(_ aLocation:CLLocationCoordinate2D,_ aName:String,_ aKey:String){
+//        let tObserverList=mUpdateLocationObserver!
+//        for tObserver in tObserverList{
+//            tObserver.0(aLocation,aName,aKey)
+//        }
+//    }
+}
